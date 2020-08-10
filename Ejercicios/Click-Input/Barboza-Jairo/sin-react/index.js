bfab4e4e16165c583c858edb4d979bf3e9718d7f
jairo <jairobarboza159@gmail.com>
2020-08-10 16:13:13 -0300

    añadiendo mi carpeta

---
+++ Ejercicios/Click-Input/Barboza-Jairo/sin-react/index.js
@@ -0,0 +1,9 @@
+var boton = document.getElementById("boton");
+    boton.addEventListener("click", Ejercicio1);
+
+function Ejercicio1(){
+
+    var texto = document.getElementById("input").value;
+    alert(texto);
+    
+}
